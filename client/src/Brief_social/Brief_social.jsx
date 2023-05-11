1de48f1f6d5f3225d2a97f35f188d88349707845
QuannBluee <quannguyenthanh558@gmail.com>
2023-05-11 10:35:47 +0700

    fix (client): insecure in target _blank

--- client/src/Brief_social/Brief_social.jsx
+++ client/src/Brief_social/Brief_social.jsx
@@ -1,34 +1,53 @@
-import './style.css';
+import "./style.css";
 import React from "react";
-import avatar from '../../src/pic/Avatar.jpg';
+import avatar from "../../src/pic/Avatar.jpg";
 
 function Home() {
-    return (
-        <div className="brief-card d-flex flex-column justify-content-center align-items-center pt-5">
-            <div className="avatar mt-4 mx-4" >
-                <img src={avatar} alt="avatar" />
-            </div>
+	return (
+		<div className="brief-card d-flex flex-column justify-content-center align-items-center pt-5">
+			<div className="avatar mt-4 mx-4">
+				<img src={avatar} alt="avatar" />
+			</div>
 
-            <h3 className="myname mb-0 mt-3">Thanh Quân</h3>
-            <h4 className="mb-5">S&nbsp;t&nbsp;u&nbsp;d&nbsp;e&nbsp;n&nbsp;t</h4>
+			<h3 className="myname mb-0 mt-3">Thanh Quân</h3>
+			<h4 className="mb-5">S&nbsp;t&nbsp;u&nbsp;d&nbsp;e&nbsp;n&nbsp;t</h4>
 
-            <div className="icons row p-2 mt-5 d-flex justify-content-center">
-                <a href="https://github.com/QuanBlue" target="_blank" >
-                    <i className="fab fa-github-alt icon-github"></i>
-                </a>
-                <a href="https://www.linkedin.com/in/quanblue/" target="_blank" >
-                    <i className="fab fa-linkedin-in icon-linkedin" aria-hidden="true"></i>
-                </a>
+			<div className="icons row p-2 mt-5 d-flex justify-content-center">
+				<a
+					href="https://github.com/QuanBlue"
+					rel="noreferrer"
+					target="_blank"
+				>
+					<i className="fab fa-github-alt icon-github"></i>
+				</a>
+				<a
+					href="https://www.linkedin.com/in/quanblue/"
+					rel="noreferrer"
+					target="_blank"
+				>
+					<i
+						className="fab fa-linkedin-in icon-linkedin"
+						aria-hidden="true"
+					></i>
+				</a>
 
-                <a href="https://www.facebook.com/QuanBlueee/" target="_blank">
-                    <i className="fab fa-facebook-f icon-fb"></i>
-                </a>
-                <a href="https://www.instagram.com/quanblueee/" target="_blank">
-                    <i className="fab fa-instagram icon-insta"></i>
-                </a>
-            </div>
-        </div >
-    );
+				<a
+					href="https://www.facebook.com/QuanBlueee/"
+					rel="noreferrer"
+					target="_blank"
+				>
+					<i className="fab fa-facebook-f icon-fb"></i>
+				</a>
+				<a
+					href="https://www.instagram.com/quanblueee/"
+					rel="noreferrer"
+					target="_blank"
+				>
+					<i className="fab fa-instagram icon-insta"></i>
+				</a>
+			</div>
+		</div>
+	);
 }
 
 export default Home;
